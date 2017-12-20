dd05bcc0bac53d80cd941276cb39adb0ea2eca55
Boris Golomb <boris_golomb@epam.com>
2017-12-20 11:54:29 +0300

    Add test ng test with data driven approach

--- src/test/Java/TestNG/SubTest.java
+++ src/test/Java/TestNG/SubTest.java
@@ -1,4 +1,23 @@
 package TestNG;
 
-public class SubTest {
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SubTest extends BaseTest{
+    @Test(dataProvider = "subDp", groups = "sub")
+    public void oneValueMinusAnother(long a, long b, long expected) {
+        long result = calculator.sub(a, b);
+        assertEquals(result, expected, "Invalid result of operation");
+    }
+    @DataProvider(name = "subDp")
+    public Object[][] valuesForSub() {
+        return new Object[][]{
+                {20, 15, 5},
+                {9, 6, 3},
+                {1, -10, 11},
+                {0, 0, 0}
+        };
+    }
 }
